165e823072823f6fd40d2a239480ce645a606c24
ABodey <A.S.Bodey@gmail.com>
2017-02-15 00:45:11 -0800

    add functioality to questions

--- game.js
+++ game.js
@@ -15,8 +15,14 @@ function userName(){
   // if ((invitation.toLowerCase() === "yes")||(invitation.toLowerCase() === "y")){
   //   alert("Great!")
 
-  console.log("Ask 3 questions with if/else alert messages");
-  console.log("Added  score system");
+  // console.log("Ask 3 questions with if/else alert messages");
+  // console.log("Added  score system");
+
+  console.log("add global varibles");
+  var baseScore = 0;
+//  var answerParagraph = document.getElementById("answer-location");
+  var result=("")
+
 
   function scoreKeeper(baseScore){
     //var baseScore = (0);
@@ -30,7 +36,7 @@ function userName(){
     question2();
     question3();
     question4();
-    //question5();
+    question5();
     console.log("ggame ran");
   };
 
@@ -42,11 +48,13 @@ function userName(){
       //var baseScore = (0);
       //baseScore++;
       //return scoreKeeper;
-      var a1 = ("Correct, his name is Bubdow")
-      return answerParagraph.innerHTML = a1 ; //("Correct, his name is Bubdow");
+      result += ("Correct, his name is Bubdow");
+      return answerParagraph.innerHTML = result; //("Correct, his name is Bubdow");
     }
     else {
-      return answerParagraph.innerHTML = ("Andrew does have a dog, his name is Bubdow")
+      result += ("Andrew does have a dog, his name is Bubdow");
+        var answerParagraph = document.getElementById("answer-location");
+      return answerParagraph.innerHTML = result;
     };
   };
 
@@ -54,13 +62,15 @@ function userName(){
 
     var q2 = prompt("What was the Project from Code 101 called?","Enter code name here");
     if ((q2.toLowerCase()=== "fit squirrels")||(q2.toLowerCase()=== "squirrels")){
-      var a2 = ("The page name was Fit Squirrels");
+  var answerParagraph = document.getElementById("answer-location");
     //  baseScore++;
-    a1 += a2;
-    return answerParagraph.innerHTML = a1 ;
+    result += ("<br>Correct,The page name was Fit Squirrels");
+    return answerParagraph.innerHTML = result;
     }
     else {
-      alert("Not the answer I am looking for, see the bottom of the page.")
+      result += ("<br>Not the answer I am looking for, see the bottom of the page.");
+        var answerParagraph = document.getElementById("answer-location");
+     return answerParagraph.innerHTML = result;
     }
   };
   // var userScore = alert("Your current score is "+ baseScore + " out of 5." );
@@ -68,11 +78,16 @@ function userName(){
   function question3(){
     var q3 = prompt("Where was Andrew born?","Enter the city name");
     if ((q3.toLowerCase() === "seattle")||(q3.toLowerCase() === "sea")){
-      alert("Correct");
-    //  baseScore++;
-    }
+      var answerParagraph = document.getElementById("answer-location");
+        //  baseScore++;
+        result += ("<br>Correct Andrew was born in Seattle");
+        return answerParagraph.innerHTML = result;
+        }
     else {
-      alert("Not the answer I am looking for, read the first part for a clue")
+        var answerParagraph = document.getElementById("answer-location");
+      result += ("<br>Not the answer I am looking for, read the first part for a clue");
+
+     return answerParagraph.innerHTML = result;
     }
   };
   //  var userScore = alert("Your current score is "+ baseScore + " out of 5." );
@@ -81,20 +96,28 @@ function userName(){
   // Fixed switch, remember if case is true than it runs
   function question4(){
     var q4 = prompt("How many times has Andrew been to China?");
-    var x=parseInt(question4);
+    var x=parseInt(q4);
     switch (true) {
       case (x === 3):
-      alert("Correct!");
+    result += ("<br>Correct, Andrew has been to China 3 times so far.");
+    var answerParagraph = document.getElementById("answer-location");
+ return answerParagraph.innerHTML = result;
     //  baseScore++;
       break;
       case (x <=2):
-      alert("You guessed too low");
+        result += ("<br>You guessed too low");
+      var answerParagraph = document.getElementById("answer-location");
+   return answerParagraph.innerHTML = result;
       break;
       case (x >=4):
-      alert("You guessed too high");
+      result += ("<br>You guessed too high");
+      var answerParagraph = document.getElementById("answer-location");
+   return answerParagraph.innerHTML = result;
       break;
       default:
-      alert("Please enter a number value");
+      result += ("<br>Please enter a number value, to guess an age.");
+      var answerParagraph = document.getElementById("answer-location");
+   return answerParagraph.innerHTML = result;
     }
   };
   //  var userScore = alert("Your current score is "+ baseScore + " out of 5." );
@@ -109,14 +132,18 @@ function userName(){
       }else if(q5>32){
         q5 = prompt("You guessed too high, guess again.","Enter a number");
       }
+    }
       // alert("Correct");
       // baseScore++;
-      ("Correct");
+      result += ("<br>Correct");
+      var answerParagraph = document.getElementById("answer-location");
+   return answerParagraph.innerHTML = result;
+
     //  baseScore++;
 
       //  var userScore = ("Your current score is "+ baseScore + " out of 5." );
       console.log("end of Guessing Game");
-    }
+
   };
 
 
